75589c19b3ca2728031164b49d80fa6d53e50c21
Onowuckc <ijomahchinazaaugustine@gmail.com>
2024-10-20 16:21:45 +0100

    Initial Commit

---
+++ src/test/java/com/hms/managementsys/ManagementsysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hms.managementsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
